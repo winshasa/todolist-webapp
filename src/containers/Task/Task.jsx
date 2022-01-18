c4e22661f160e144ab39b8bca57f628d1cf6cc46
Gael Wamba <gaelmusi0@gmail.com>
2022-01-18 22:43:57 +0200

    Connect API to frontend app

--- src/containers/Task/Task.jsx
+++ src/containers/Task/Task.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const URL = "http://127.0.0.1:8003/";
 
 const Task = () => {
-  const [task, setTask] = useState({
+  const initialValue = {
     title: "",
     completed: 0,
-    // description: ''
-  });
+    description: "",
+  };
+  const [task, setTask] = useState(initialValue);
   const [tasks, setTasks] = useState([]);
   const [error, setError] = useState("");
 
   const handleTaskChange = (e) => {
+    setError("");
     let task = {
       title: e.target.value,
       completed: 0,
@@ -17,40 +22,99 @@ const Task = () => {
     setTask(task);
   };
 
-  const handleAddTask = () => {
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: URL + "api/tasks",
+    })
+      .then((response) => {
+        setTasks(response.data.data);
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
+  }, []);
+
+  const handleAddTask = async () => {
     if (task.title !== "") {
-      let arrTask = [...tasks, task];
-      setTasks(arrTask);
+      //Ajouter un tache dans la base de donnes
+      await axios({
+        method: "post",
+        url: URL + "api/tasks",
+        data: task,
+      })
+        .then((response) => {
+          let arrTask = [...tasks, response.data.data];
+          setTasks(arrTask);
+          setTask(initialValue);
+        })
+        .catch((error) => {
+          console.log("error: ", error);
+        });
     } else {
       setError("Can't input empty task value");
     }
   };
 
-  const handleDone = (event, index) => {
+  const handleDone = async (event, taskId) => {
     event.preventDefault();
     let task_copy = [...tasks];
-    let completed = task_copy[index].completed;
-    task_copy[index].completed = !completed ? 1 : 0;
-    setTasks(task_copy);
+    let updated = task_copy.filter((t) => t.id === taskId)[0];
+    updated.completed = !updated.completed;
+    console.log("updated: ", updated);
+
+    await axios({
+      method: "patch",
+      url: URL + "api/tasks/" + taskId,
+      data: {
+        ...updated,
+        description: "",
+      },
+    })
+      .then((response) => {
+        let task_copy = [...tasks];
+        task_copy.map((t) => {
+          if (t.id === taskId) {
+            t = response.data.data;
+          }
+          return t;
+        });
+        setTasks(task_copy);
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
   };
 
-  const handleRemove = (index) => {
-    let task_copy = [...tasks];
-    task_copy.splice(index, 1);
-    setTasks(task_copy);
+  const handleRemove = async (taskId) => {
+    await axios({
+      method: "delete",
+      url: URL + "api/tasks/" + taskId,
+    })
+      .then((response) => {
+        let task_copy = [...tasks];
+        let updated = task_copy.filter((t) => t.id === taskId)[0];
+        let index = task_copy.indexOf(updated);
+        task_copy.splice(index, 1);
+        setTasks(task_copy);
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+      });
   };
 
   return (
     <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
       <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
         <div className="mb-4">
-          <h1 className="text-grey-darkest">Todo List</h1>
+          <h1 className="text-gray-900">Todo List</h1>
           {error !== "" ? <b className="text-red-600">{error}</b> : ""}
           <div className="flex mt-4">
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-gray-900"
               placeholder="Add Todo"
               onChange={handleTaskChange}
+              value={task && task.title}
             />
             <button
               className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal"
@@ -60,27 +124,33 @@ const Task = () => {
           </div>
         </div>
         <div>
-          {tasks.map((task, index) => {
+          {tasks.map((task) => {
             return (
-              <div key={index} className="flex mb-4 items-center">
+              <div key={task.id} className="flex mb-4 items-center">
                 <p
                   className={
-                    "w-full text-grey-darkest " +
-                    (task.completed === 1 ? "line-through" : "")
+                    "w-7/12 text-gray-darkest " +
+                    (task.completed ? "line-through text-green" : "")
                   }>
                   {task.title}
                 </p>
-                <button
-                  className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green"
-                  onClick={(event) => handleDone(event, index)}>
-                  {task.completed === 1 ? "Pas finis" : "Finis"}
-                </button>
-
-                <button
-                  className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"
-                  onClick={() => handleRemove(index)}>
-                  Remove
-                </button>
+                <div className="flex justify-end w-5/12">
+                  <button
+                    className={
+                      "flex-no-shrink p-2 mr-2 border-2 rounded hover:text-white " +
+                      (task.completed
+                        ? "text-gray hover:bg-gray border-gray"
+                        : "text-green border-green hover:bg-green")
+                    }
+                    onClick={(event) => handleDone(event, task.id)}>
+                    {task.completed ? "Not Done" : "Done"}
+                  </button>
+                  <button
+                    className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red"
+                    onClick={() => handleRemove(task.id)}>
+                    Remove
+                  </button>
+                </div>
               </div>
             );
           })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,85 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: {
+          ...colors.green,
+          DEFAULT: "#28a745",
+        },
+        red: {
+          ...colors.red,
+          DEFAULT: "#dc3545",
+        },
+        yellow: {
+          ...colors.yellow,
+          DEFAULT: "#ffc107",
+        },
+        blue: {
+          ...colors.blue,
+          DEFAULT: "#17a2b8",
+        },
+        purple: {
+          ...colors.violet,
+          DEFAULT: "#8B5CF6",
+        },
+        pink: {
+          ...colors.pink,
+          DEFAULT: "#EC4899"
+        },
+        indigo: {
+          ...colors.indigo,
+          DEFAULT: "#6366F1"
+        },
+        gray: {
+          ...colors.gray,
+          DEFAULT: "#6B7280"
+        },
+        orange: {
+          ...colors.yellow,
+          DEFAULT: "#FF8B00"
+        },
+        social: {
+          facebook: "#3b5998",
+          twitter: "#1da1f2",
+          pinterest: "#bd081c",
+          whatsapp: "#25d366",
+        },
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      black: colors.black,
+      white: colors.white,
+      primary: {
+        DEFAULT: "#145388",
+        100: "#F3F6F9",
+        200: "#C4D4E1",
+        300: "#A1BACF",
+        400: "#5B87AC",
+        500: "#145388",
+        600: "#124B7A",
+        700: "#0C3252",
+        800: "#09253D",
+        900: "#061929",
+      },
+      secondary: "#555555",
+      gray: {
+        100: "#f8f8f8",
+        200: "#eeeeee",
+        300: "#dddddd",
+        400: "#cccccc",
+        500: "#aaaaaa",
+        600: "#888888",
+        700: "#555555",
+        800: "#242526",
+        900: "#151515",
+      },
+    },
   },
   plugins: [],
 }
